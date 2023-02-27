1bb7396db2516bbc744922dd783fc5db68e24623
xushaoping <xusp@yunxi.tv>
2023-02-27 15:29:20 +0800

    canvas code learn

--- src/components/video-canvas.vue
+++ src/components/video-canvas.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <video id="video" src="../assets/test.mp4" controls />
+    <video id="video" src="../assets/test.mp4" controls width="180" />
+    <button @click="captureScreen">屏幕分享</button>
+    <button @click="captureCamera">摄像头采集</button>
   </div>
   <div>
-    <canvas id="canvas" width="320" height="180" />
+    <canvas id="canvas" width="1200" height="720" />
   </div>
 </template>
 
@@ -23,19 +25,39 @@ onMounted(() => {
   })
 })
 
-const captureStream = () => {
-  if (videoEl.paused || videoEl.ended) return;
-  const stream = videoEl.captureStream(30);
-  const audioTrack = stream.getAudioTracks()[0];
-  const videoTrack = stream.getVideoTracks()[0];
-
-  console.log(videoTrack, 111)
-  console.log(audioTrack, 222)
-  paintVideo()
+let videoStream: MediaStream;
+const captureCamera = () => {
+  navigator.mediaDevices.getUserMedia({ audio: false, video: true }).then(stream => {
+    videoStream = stream;
+  }).catch(error => {
+    console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+  })
 }
 
-const paintVideo = () => {
-  ctx.drawImage(videoEl, 0, 0, 320, 180)
+let screenStream: MediaStream;
+const captureScreen = async () => {
+  try {
+    screenStream = await navigator.mediaDevices.getDisplayMedia()
+    paintScreen();
+  } catch (err) {
+    console.error("Error: " + err);
+  }
+}
+
+const paintScreen = () => {
+  console.log(screenStream, 11)
+  const videoTrack = screenStream.getVideoTracks()[0];
+  // 流无法绘制到 canvas ，前端目前通过必须转成video
+  // ctx.drawImage(videoTrack, 960, 540, 320, 180)
+}
+
+const captureStream = () => {
+  if (videoEl.paused || videoEl.ended) return;
+  ctx.drawImage(videoEl, 0, 0, 1200, 720)
+  requestAnimationFrame(captureStream);
+  // const stream = videoEl.captureStream(30);
+  // const audioTrack = stream.getAudioTracks()[0];
+  // const videoTrack = stream.getVideoTracks()[0];
 }
 
 </script>
